e336e08974159be6e68b8d13faf3ac8c0d8ddce6
russellphysics <32598683+russellphysics@users.noreply.github.com>
2023-06-14 09:16:32 -0700

    initial commit

---
+++ gain_analog_linearity.py
@@ -0,0 +1,15 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+from scipy.optimize import curve_fit
+import argparse
+
+def main(**kwargs):
+    print('haha')
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    main(**vars(args))
